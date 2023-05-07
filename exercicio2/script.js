0531689243c8d7ad65edb40a804d11148c4dc281
Rodrigo Vitor Martins Godoy <rodrigo.godoy4556@gmail.com>
2023-05-06 21:01:10 -0300

    Exercícios de Feedback da aula Variaveis

---
+++ exercicio2/script.js
@@ -0,0 +1,15 @@
+const chuva = "Está chovendo hoje?"
+const cabelo = "Seu cabelo é comprido?"
+const alien = "Você é um alien?"
+
+const respostaChuva = prompt(chuva)
+const respostaCabelo = prompt(cabelo)
+const respostaAlien = prompt(alien)
+
+console.log(chuva, '-', respostaChuva);
+console.log(cabelo, '-', respostaCabelo);
+console.log(alien, '-', respostaAlien);
+
+
+
+
